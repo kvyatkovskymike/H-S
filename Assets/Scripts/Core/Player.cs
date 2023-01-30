2969a374b34acba9a514959965c4d9f6f87dcb79
mike <mike@Noutbuk-mike.IGD_Rostelecom>
2023-01-30 19:09:43 +0000

    player message

--- Assets/Scripts/Core/Player.cs
+++ Assets/Scripts/Core/Player.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float _health;
+    private string _characterName;
     void Start()
     {
-        
+                
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Player status norm");
     }
 }
